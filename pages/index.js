58d023349ab0ed2615528c00d10970c8577912c8
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-12-14 20:43:58 -0300

    add global style

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Link from "../src/components/Link";
 
 export default function HomePage() {
+  function Title({ children, as }) {
+    const Tag = as;
+
+    return (
+      <>
+        <Tag>{children}</Tag>
+        <style jsx>{`
+          ${Tag} {
+            color: purple;
+          }
+        `}</style>
+      </>
+    );
+  }
+
   return (
     <div>
-      <h1>Jobs Project</h1>
+      <Title as="h1">Jobs Project</Title>
       <Link href={"/faq"}>FAQ</Link>
     </div>
   );
