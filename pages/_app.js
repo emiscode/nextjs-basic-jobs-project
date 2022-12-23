99e5261fe117fa14262b39513e3262f806f5a3bb
Emilio Scodeler <emilio.scodeler@solides.com.br>
2022-12-23 10:55:51 -0300

    feat: added google site verification

--- pages/_app.js
+++ pages/_app.js
@@ -30,6 +30,10 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Bitter&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="google-site-verification"
+          content="uM1nw-9P0wkcazJVhzBnTTrDbrLtjb6xMpFk-uCnjEg"
+        />
       </PageTitle>
       <GlobalStyle />
       <Component {...pageProps} />
